a4bd5da75e8b12f857cd09a5e095042c1d9c420b
Carlos William <carloswilliamds@gmail.com>
2019-11-29 20:25:17 -0200

    start calculadora

---
+++ calculadora/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import Calculator from './main/calculator';
+
+function App() {
+  return (
+    <div className="box-calculator">
+      <h1 className="title-calculator">Calculadora</h1>
+      <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
